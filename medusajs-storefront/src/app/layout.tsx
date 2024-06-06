5d16a7a09467bed39d4780f89727626c35208338
ShiftLabs <joshua@Shiftlabs.xyz>
2024-06-06 23:23:18 +0200

    update meta tags

--- medusajs-storefront/src/app/layout.tsx
+++ medusajs-storefront/src/app/layout.tsx
@@ -5,6 +5,14 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
 
 export const metadata: Metadata = {
   metadataBase: new URL(BASE_URL),
+  category: "luxury hair",
+  description:
+    "Luxury hair for boss ladies. Best in class wigs and extensions. 100% human hair, bone straight, curls. Free shipping. Easy returns. Shop now!",
+  abstract: "Online store for Hairvenbeauty.Place orders for your customised wigs and hair extensions. Buy gift Cards for bae and your loved ones",
+  applicationName: "HairvenBeauty",
+  creator: "ShiftLabs",
+  keywords: "hair, wigs, extensions, beauty, luxury, boss ladies, human hair, free shipping, easy returns, shop now, gift cards, bae, loved ones, customised wigs, online store",
+  robots: "index,follow",
 }
 
 export default function RootLayout(props: { children: React.ReactNode }) {
