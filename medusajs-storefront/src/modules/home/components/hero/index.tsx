eef8a39d4e1e0b4dc71793453e21141880c358e3
ShiftLabs <joshua@Shiftlabs.xyz>
2024-06-06 21:09:35 +0200

    welcome loader

--- medusajs-storefront/src/modules/home/components/hero/index.tsx
+++ medusajs-storefront/src/modules/home/components/hero/index.tsx
@@ -4,19 +4,19 @@ import { Button, Heading } from "@medusajs/ui"
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
+      <div className="absolute inset-0 z-10 flex flex-col items-center justify-center gap-6 text-center small:p-32">
         <span>
           <Heading
             level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
+            className="text-3xl font-normal leading-10 text-ui-fg-base"
           >
-            Ecommerce Starter Template
+            Welcome to the 
           </Heading>
           <Heading
             level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
+            className="text-3xl font-normal leading-10 text-ui-fg-subtle"
           >
-            Powered by Medusa and Next.js
+            Hairven Beauty store
           </Heading>
         </span>
         <a
@@ -24,7 +24,7 @@ const Hero = () => {
           target="_blank"
         >
           <Button variant="secondary">
-            View on GitHub
+            Get Shopping
             <Github />
           </Button>
         </a>
