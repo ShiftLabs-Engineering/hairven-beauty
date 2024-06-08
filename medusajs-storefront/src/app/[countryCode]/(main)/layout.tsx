f621c5084fe22161ab621c791076290246e4cba7
ShiftLabs <joshua@Shiftlabs.xyz>
2024-06-08 11:00:58 +0200

    more metadata context updates

--- medusajs-storefront/src/app/[countryCode]/(main)/layout.tsx
+++ medusajs-storefront/src/app/[countryCode]/(main)/layout.tsx
@@ -7,6 +7,7 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
 
 export const metadata: Metadata = {
   metadataBase: new URL(BASE_URL),
+  title: "Hairven Beauty Store",
 }
 
 export default async function PageLayout(props: { children: React.ReactNode }) {
--- medusajs-storefront/src/app/layout.tsx
+++ medusajs-storefront/src/app/layout.tsx
@@ -4,14 +4,17 @@ import "styles/globals.css"
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
 
 export const metadata: Metadata = {
+  title: "HairvenBeauty Store | Luxury Hair for Boss Ladies",
   metadataBase: new URL(BASE_URL),
-  category: "luxury hair",
+  category: "wigs,hair extensions,wig rejuvenation and treatment,customised wigs,100% human hair",
   description:
     "Luxury hair for boss ladies. Best in class wigs and extensions. 100% human hair, bone straight, curls. Free shipping. Easy returns. Shop now!",
-  abstract: "Online store for Hairvenbeauty.Place orders for your customised wigs and hair extensions. Buy gift Cards for bae and your loved ones",
+  abstract:
+    "Online store for Hairvenbeauty.Place orders for your customised wigs and hair extensions. Buy gift Cards for bae and your loved ones",
   applicationName: "HairvenBeauty",
   creator: "ShiftLabs",
-  keywords: "hair, wigs, extensions, beauty, luxury, boss ladies, human hair, free shipping, easy returns, shop now, gift cards, bae, loved ones, customised wigs, online store",
+  keywords:
+    "hairvenbeauty,south africa,johannesburg,bone straight, luxury haur,hair, wigs, extensions, beauty, luxury, boss ladies, human hair, free shipping, easy returns, shop now, gift cards, bae, loved ones, customised wigs, online store",
   robots: "index,follow",
 }
 
